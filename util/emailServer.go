9e248b968c46c1457cc5b7c1975a2069c8d218b6
liuzhiyuhx <1225053838@qq.com>
2023-09-19 17:22:34 +0800

    SendEmail

--- util/emailServer.go
+++ util/emailServer.go
@@ -1,28 +1,35 @@
 package util
 
 import (
-	"fmt"
 	"github.com/ethereum/BGService/config"
-	"github.com/jordan-wright/email"
-	"net/smtp"
+	"gopkg.in/gomail.v2"
+	"log"
 )
 
 func SendEmail(config *config.Config, to []string, subject string, body string) error {
 	// 发送电子邮件
 	// 配置SMTP服务器信息
-	smtpServer := config.Email.SmtpServer
-	smtpPort := config.Email.SmtpPort
-	smtpUsername := config.Email.SmtpUsername
-	smtpPassword := config.Email.SmtpPassword
+	mailTitle := subject //邮件标题
+	mailBody := body     //邮件内容,可以是html
 
-	e := email.NewEmail()
-	e.From = smtpUsername
-	e.To = to
-	e.Subject = subject
-	e.Text = []byte(body)
+	//接收者邮箱列表
+	mailTo := to
 
-	// 连接到SMTP服务器
-	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
-	err := e.Send(fmt.Sprintf("%s:%d", smtpServer, smtpPort), auth)
-	return err
+	m := gomail.NewMessage()
+	m.SetHeader("From", config.Email.SmtpUsername) //发送者腾讯企业邮箱账号
+	m.SetHeader("To", mailTo...)                   //接收者邮箱列表
+	m.SetHeader("Subject", mailTitle)              //邮件标题
+	m.SetBody("text/html", mailBody)               //邮件内容,可以是html
+
+	//发送邮件服务器、端口、发件人账号、发件人密码
+	//服务器地址和端口是腾讯的
+	d := gomail.NewDialer(config.Email.SmtpServer, config.Email.SmtpPort, config.Email.SmtpUsername, config.Email.SmtpPassword)
+	if err := d.DialAndSend(m); err != nil {
+		log.Println("send mail failed", err)
+		return err
+	}
+
+	log.Println("success")
+
+	return nil
 }
